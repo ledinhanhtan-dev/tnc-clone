f59708a11e04726e7797d0aff427db7c04fa1bf5
Oga <ledinhanhtan.dev@gmail.com>
2021-09-28 00:55:13 +0700

    refactor: update banner links

--- src/app/home/models/home-banner.model.ts
+++ src/app/home/models/home-banner.model.ts
@@ -5,20 +5,72 @@ export interface HomeBanner {
 }
 
 export const heroBanners: HomeBanner[] = [
-  { link: '#', image: 'http://localhost:3000/image/banner-01.jpg', alt: '' },
-  { link: '#', image: 'http://localhost:3000/image/banner-02.jpg', alt: '' },
-  { link: '#', image: 'http://localhost:3000/image/banner-03.jpg', alt: '' },
-  { link: '#', image: 'http://localhost:3000/image/banner-04.jpg', alt: '' },
-  { link: '#', image: 'http://localhost:3000/image/banner-05.jpg', alt: '' },
-  { link: '#', image: 'http://localhost:3000/image/banner-06.jpg', alt: '' },
-  { link: '#', image: 'http://localhost:3000/image/banner-07.jpg', alt: '' },
-  { link: '#', image: 'http://localhost:3000/image/banner-08.jpg', alt: '' },
-  { link: '#', image: 'http://localhost:3000/image/banner-09.jpg', alt: '' },
+  {
+    link: '#',
+    image: 'http://localhost:3000/image/banner/banner-01.jpg',
+    alt: '',
+  },
+  {
+    link: '#',
+    image: 'http://localhost:3000/image/banner/banner-02.jpg',
+    alt: '',
+  },
+  {
+    link: '#',
+    image: 'http://localhost:3000/image/banner/banner-03.jpg',
+    alt: '',
+  },
+  {
+    link: '#',
+    image: 'http://localhost:3000/image/banner/banner-04.jpg',
+    alt: '',
+  },
+  {
+    link: '#',
+    image: 'http://localhost:3000/image/banner/banner-05.jpg',
+    alt: '',
+  },
+  {
+    link: '#',
+    image: 'http://localhost:3000/image/banner/banner-06.jpg',
+    alt: '',
+  },
+  {
+    link: '#',
+    image: 'http://localhost:3000/image/banner/banner-07.jpg',
+    alt: '',
+  },
+  {
+    link: '#',
+    image: 'http://localhost:3000/image/banner/banner-08.jpg',
+    alt: '',
+  },
+  {
+    link: '#',
+    image: 'http://localhost:3000/image/banner/banner-09.jpg',
+    alt: '',
+  },
 ];
 
 export const subBanners: HomeBanner[] = [
-  { link: '#', image: 'http://localhost:3000/image/store-asus.jpg', alt: '' },
-  { link: '#', image: 'http://localhost:3000/image/store-lg.jpg', alt: '' },
-  { link: '#', image: 'http://localhost:3000/image/store-acer.jpg', alt: '' },
-  { link: '#', image: 'http://localhost:3000/image/store-nzxt.jpg', alt: '' },
+  {
+    link: '#',
+    image: 'http://localhost:3000/image/banner/store-asus.jpg',
+    alt: '',
+  },
+  {
+    link: '#',
+    image: 'http://localhost:3000/image/banner/store-lg.jpg',
+    alt: '',
+  },
+  {
+    link: '#',
+    image: 'http://localhost:3000/image/banner/store-acer.jpg',
+    alt: '',
+  },
+  {
+    link: '#',
+    image: 'http://localhost:3000/image/banner/store-nzxt.jpg',
+    alt: '',
+  },
 ];
