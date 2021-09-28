be963ad299b81525088ab1cf73cc93c2cf931997
Oga <ledinhanhtan.dev@gmail.com>
2021-09-29 00:35:54 +0700

    refactor: add router link in header

--- src/app/core/header/header.module.ts
+++ src/app/core/header/header.module.ts
@@ -4,10 +4,11 @@ import { SharedModule } from '@shared/shared.module';
 import { HeaderComponent } from './header.component';
 import { MegaMenuModule } from '@core/mega-menu/mega-menu.module';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, components],
-  imports: [CommonModule, SharedModule, MegaMenuModule],
+  imports: [CommonModule, SharedModule, MegaMenuModule, RouterModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
