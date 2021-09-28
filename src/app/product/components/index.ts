cc0bf2218036b752dcddacfa37eb8b624229c68d
Oga <ledinhanhtan.dev@gmail.com>
2021-09-29 00:31:37 +0700

    refactor: add product/components/index.ts

--- src/app/product/components/index.ts
+++ src/app/product/components/index.ts
@@ -1,7 +1,25 @@
-import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductCtaComponent } from './product-cta/product-cta.component';
+import { ProductInfoComponent } from './product-info/product-info.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductRatingComponent } from './product-rating/product-rating.component';
+import { ProductGalleryComponent } from './product-gallery/product-gallery.component';
+import { ProductActionsComponent } from './product-actions/product-actions.component';
+import { ProductTopToolbarComponent } from './product-top-toolbar/product-top-toolbar.component';
+import { ProductQuantityComponent } from './product-quantity/product-quantity.component';
 import { ProductsSwiperComponent } from './products-swiper/products-swiper.component';
+import { ProductPolicyComponent } from './product-policy/product-policy.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 export const components: any[] = [
-  ProductCardComponent,
+  ProductCtaComponent,
+  ProductInfoComponent,
+  ProductDetailComponent,
+  ProductRatingComponent,
+  ProductGalleryComponent,
+  ProductActionsComponent,
+  ProductTopToolbarComponent,
+  ProductQuantityComponent,
   ProductsSwiperComponent,
+  ProductPolicyComponent,
+  ProductCardComponent,
 ];
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,39 +1,16 @@
 import { NgModule } from '@angular/core';
-import { PricePipe } from './pipes/price.pipe';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
 import { ProductsSwiperComponent } from './components/products-swiper/products-swiper.component';
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductService } from './services/product.service';
-import { SwiperModule } from 'swiper/angular';
-
-import { components } from './components';
-import { ProductGalleryComponent } from './components/product-gallery/product-gallery.component';
 import { ProductComponent } from './product.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail.component';
-import { ProductInfoComponent } from './components/product-info/product-info.component';
-import { ProductRatingComponent } from './components/product-rating/product-rating.component';
-import { ProductActionsComponent } from './components/product-actions/product-actions.component';
-import { SharedModule } from '@shared/shared.module';
-import { ProductQuantityComponent } from './components/product-quantity/product-quantity.component';
-import { ProductPolicyComponent } from './components/product-policy/product-policy.component';
-import { ProductTopToolbarComponent } from './components/product-top-toolbar/product-top-toolbar.component';
-import { ProductCtaComponent } from './components/product-cta/product-cta.component';
+import { SwiperModule } from 'swiper/angular';
+import { PricePipe } from './pipes/price.pipe';
+import { components } from './components';
 
 @NgModule({
-  declarations: [
-    ...components,
-    PricePipe,
-    ProductGalleryComponent,
-    ProductComponent,
-    ProductDetailComponent,
-    ProductInfoComponent,
-    ProductRatingComponent,
-    ProductActionsComponent,
-    ProductQuantityComponent,
-    ProductPolicyComponent,
-    ProductTopToolbarComponent,
-    ProductCtaComponent,
-  ],
+  declarations: [...components, PricePipe, ProductComponent],
   imports: [CommonModule, SharedModule, SwiperModule, ProductRoutingModule],
   exports: [ProductsSwiperComponent],
   providers: [ProductService],
