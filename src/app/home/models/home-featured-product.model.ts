0c0ddf75b9d0d5da177747034cbccb7e51bffb94
Oga <ledinhanhtan.dev@gmail.com>
2021-09-29 21:51:49 +0700

    refactor: add router link to home-featured-prods

--- src/app/home/models/home-featured-product.model.ts
+++ src/app/home/models/home-featured-product.model.ts
@@ -1,33 +1,33 @@
 export interface HomeFeaturedProduct {
   image: string;
   alt: string;
-  link: string;
+  idName: string;
 }
 
 export const homeFeaturedProducts: HomeFeaturedProduct[] = [
   {
     image: 'http://localhost:3000/image/featured/featured-product-1.jpg',
     alt: '',
-    link: '#',
+    idName: '#',
   },
   {
     image: 'http://localhost:3000/image/featured/featured-product-2.jpg',
     alt: '',
-    link: '#',
+    idName: '#',
   },
   {
     image: 'http://localhost:3000/image/featured/featured-product-3.jpg',
     alt: '',
-    link: '#',
+    idName: '#',
   },
   {
     image: 'http://localhost:3000/image/featured/featured-product-4.jpg',
     alt: '',
-    link: '#',
+    idName: '#',
   },
   {
     image: 'http://localhost:3000/image/featured/featured-product-5.jpg',
     alt: '',
-    link: '#',
+    idName: '#',
   },
 ];
