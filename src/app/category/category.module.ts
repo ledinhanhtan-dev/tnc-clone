f5bb9ff9d84f4dcd5b2e7000f316afd7af71bdab
Oga <ledinhanhtan.dev@gmail.com>
2021-10-07 02:33:12 +0700

    refactor(category): add components index file

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -3,14 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './category.component';
 import { SharedModule } from '@shared/shared.module';
 import { CategoryRoutingModule } from './category-routing.module';
-import { CatFilterComponent } from './components/cat-filter/cat-filter.component';
-import { CatListComponent } from './components/cat-list/cat-list.component';
-import { CatFilterBlockComponent } from './components/cat-filter-block/cat-filter-block.component';
-import { CatFilterItemComponent } from './components/cat-filter-item/cat-filter-item.component';
-import { CatDescriptionComponent } from './components/cat-description/cat-description.component';
+import { components } from './components';
 
 @NgModule({
-  declarations: [CategoryComponent, CatFilterComponent, CatListComponent, CatFilterBlockComponent, CatFilterItemComponent, CatDescriptionComponent],
+  declarations: [...components, CategoryComponent],
   imports: [CommonModule, SharedModule, CategoryRoutingModule],
 })
 export class CategoryModule {}
---
+++ src/app/category/components/index.ts
@@ -0,0 +1,13 @@
+import { CatDescriptionComponent } from './cat-description/cat-description.component';
+import { CatFilterComponent } from './cat-filter/cat-filter.component';
+import { CatFilterBlockComponent } from './cat-filter-block/cat-filter-block.component';
+import { CatFilterItemComponent } from './cat-filter-item/cat-filter-item.component';
+import { CatListComponent } from './cat-list/cat-list.component';
+
+export const components: any[] = [
+  CatDescriptionComponent,
+  CatFilterComponent,
+  CatFilterBlockComponent,
+  CatFilterItemComponent,
+  CatListComponent,
+];
